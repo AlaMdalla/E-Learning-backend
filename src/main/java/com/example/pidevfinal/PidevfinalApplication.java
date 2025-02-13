547724be00737d093ffd260dd0cfbd5f9a4e4e8c
rayen103 <rayen.farhani@esprit.tn>
2025-02-13 10:09:52 +0100

    add commits

---
+++ src/main/java/com/example/pidevfinal/PidevfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.pidevfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidevfinalApplication.class, args);
+	}
+
+}
